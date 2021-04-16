13345ef0ff9beb1a8fa38d579a86fe3d89cba731
John Romulo Nascimento de Araujo <john.araujo@pampa.compasso>
2021-04-16 12:35:32 -0300

    configurações deresolução

--- src/utils/checkColorPixel.js
+++ src/utils/checkColorPixel.js
@@ -6,11 +6,22 @@ const url = configs.screen_shot_folder;
 async function checkColorPixel(file, coordinates) {
   const img = await loadImage(`${url}/${file}`);
   return new Promise((resolve, reject) => {
-    const canvas = createCanvas(1024, 768);
-    canvas.width = 1024;
-    canvas.height = 768;
+    const canvas = createCanvas(
+      configs.resolution.width,
+      configs.resolution.height
+    );
+    canvas.width = configs.resolution.width;
+    canvas.height = configs.resolution.height;
 
-    canvas.getContext("2d").drawImage(img, 0, 0, 1024, 768);
+    canvas
+      .getContext("2d")
+      .drawImage(
+        img,
+        0,
+        0,
+        configs.resolution.width,
+        configs.resolution.height
+      );
     var pixelData = canvas
       .getContext("2d")
       .getImageData(coordinates.x, coordinates.y, 1, 1).data;
