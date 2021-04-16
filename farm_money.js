e0d2cc85e58f98e16e2b0aa93f3c1bcaa81db8bb
John <johnromulo@Air-de-John.local>
2021-04-15 21:19:55 -0300

    farm money

--- farm_money.js
+++ farm_money.js
@@ -1,9 +1,11 @@
 const robot = require("robotjs");
-const img = require("../../../../../johnromulo/Pictures/PokeMMO Screenshots/screenshot_1618505189.png");
 
 const sleep = require("./src/utils/sleep");
 const repeat = require("./src/utils/repeat");
 const customKeyTap = require("./src/utils/customKeyTap");
+const getAllFiles = require("./src/utils/getAllFiles");
+const excludeScreenShots = require("./src/utils/excludeScreenShots");
+const checkColorPixel = require("./src/utils/checkColorPixel");
 
 const config = {
   sweet_scent_key: "5",
@@ -13,41 +15,94 @@ const config = {
 };
 
 const SPEED_STEPS = config.speed_steps;
+let pp = 20;
 
-async function run() {
-  console.log("Coloque o pokemmo em foco");
-  await sleep(5);
-  console.log("iniciando");
-
-  await repeat(2, SPEED_STEPS, async () => await customKeyTap("s"));
-  await repeat(5, SPEED_STEPS, async () => await customKeyTap("d"));
-  await repeat(6, SPEED_STEPS, async () => await customKeyTap("s"));
-  console.log("saindo do centro");
-  await sleep(3);
-  await repeat(4, SPEED_STEPS, async () => await customKeyTap("s"));
-  await repeat(5, SPEED_STEPS, async () => await customKeyTap("d"));
-  await repeat(3, SPEED_STEPS, async () => await customKeyTap("s"));
-  await repeat(10, SPEED_STEPS, async () => await customKeyTap("d"));
-
-  let num = 0;
-  const q4 = async () => {
-    if (num <= 250) {
-      console.log("pescando, num", num);
-      await repeat(1, SPEED_STEPS, async () => await customKeyTap("4"));
-      await sleep(2);
-      await repeat(1, SPEED_STEPS, async () => await customKeyTap("q"));
-      num++;
-      await q4();
+async function checkbattle() {
+  return new Promise(async (resolve, reject) => {
+    const filesToExclude = await getAllFiles();
+    if (filesToExclude.length > 0) {
+      await excludeScreenShots(filesToExclude);
     }
-  };
 
-  await q4();
+    console.log("print checkbattle");
+    await repeat(1, SPEED_STEPS, async () => await customKeyTap("0"));
 
-  console.log("esperando tempo de batalha");
-  await sleep(13.5);
+    const files = await getAllFiles();
+
+    const color = await checkColorPixel(files[0], { x: 1024, y: 1 });
+    if (color[0] === 0 && color[1] === 0 && color[2] === 0) {
+      console.log("batalha");
+      resolve(true);
+    } else {
+      console.log("andando");
+      resolve(false);
+    }
+  });
+}
+
+async function checkHorda() {
+  return new Promise(async (resolve, reject) => {
+    const filesToExclude = await getAllFiles();
+    if (filesToExclude.length > 0) {
+      await excludeScreenShots(filesToExclude);
+    }
+
+    console.log("print checkHorda");
+    await repeat(1, SPEED_STEPS, async () => await customKeyTap("0"));
+
+    const files = await getAllFiles();
+
+    const color = await checkColorPixel(files[0], { x: 150, y: 102 });
+    if (color[0] === 0 && color[1] === 0 && color[2] === 0) {
+      console.log("sem horda");
+      resolve(false);
+    } else {
+      console.log("horda");
+      resolve(true);
+    }
+  });
+}
+
+async function battle() {
+  console.log("entrando na batalha");
+  await sleep(9);
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("a"));
+  await sleep(2);
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("w"));
+  const isHorda = await checkHorda();
+  if (isHorda) {
+    await repeat(6, SPEED_STEPS, async () => await customKeyTap("d"));
+    await sleep(2);
+    await repeat(6, SPEED_STEPS, async () => await customKeyTap("s"));
+    await repeat(2, SPEED_STEPS, async () => await customKeyTap("q"));
+    await sleep(8);
+    await goToCenter();
+  }
+  console.log("atk");
   await repeat(2, SPEED_STEPS, async () => await customKeyTap("q"));
   await sleep(12);
-  console.log("teleportando para o centro");
+  const isbattle = await checkbattle();
+  if (isbattle) {
+    await sleep(8);
+    console.log("atk 2");
+    await repeat(1, SPEED_STEPS, async () => await customKeyTap("q"));
+    await repeat(1, SPEED_STEPS, async () => await customKeyTap("s"));
+    await repeat(1, SPEED_STEPS, async () => await customKeyTap("s"));
+    await sleep(12);
+  }
+  console.log("saindo da batalha");
+  pp--;
+  console.log(`restam ${pp}s`);
+  if (pp <= 0) {
+    await goToCenter();
+  } else {
+    await resetRoute();
+    await route();
+  }
+}
+
+async function goToCenter() {
+  console.log("teleportando para o centro pokemon");
   await repeat(
     1,
     SPEED_STEPS,
@@ -57,15 +112,63 @@ async function run() {
   await sleep(5);
   console.log("curando");
   await repeat(7, 1.5, async () => await customKeyTap("q"));
-
+  pp = 20;
   await sleep(1);
   await run();
 }
 
+async function resetRoute() {
+  console.log("resetando rota");
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("s"));
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("d"));
+  await sleep(4);
+}
+
+async function route() {
+  console.log("seguindo rota");
+  await repeat(4, SPEED_STEPS, async () => await customKeyTap("w"));
+  await repeat(2, SPEED_STEPS, async () => await customKeyTap("a"));
+  await repeat(3, SPEED_STEPS, async () => await customKeyTap("s"));
+  await repeat(5, SPEED_STEPS, async () => await customKeyTap("a"));
+  await repeat(2, SPEED_STEPS, async () => await customKeyTap("s"));
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("d"));
+
+  const isbattle = await checkbattle();
+  if (isbattle) {
+    await battle();
+  } else {
+    await route();
+  }
+}
+
+async function run() {
+  console.log("Coloque o pokemmo em foco");
+  await sleep(5);
+  console.log("iniciando");
+
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("s"));
+  console.log("saindo do centro");
+  await sleep(2);
+  await repeat(7, SPEED_STEPS, async () => await customKeyTap("a"));
+  await repeat(5, SPEED_STEPS, async () => await customKeyTap("w"));
+  await repeat(2, SPEED_STEPS, async () => await customKeyTap("d"));
+  await repeat(15, SPEED_STEPS, async () => await customKeyTap("w"));
+  await repeat(17, SPEED_STEPS, async () => await customKeyTap("a"));
+  await repeat(3, SPEED_STEPS, async () => await customKeyTap("w"));
+  await repeat(6, SPEED_STEPS, async () => await customKeyTap("a"));
+  await repeat(4, SPEED_STEPS, async () => await customKeyTap("s"));
+
+  console.log("iniciando rota de farm");
+  await resetRoute();
+  // checkar se está em batalha
+  const isbattle = await checkbattle();
+  if (isbattle) {
+    await battle();
+  }
+
+  await route();
+
+  console.log("fim");
+}
+
 run();
-
-// 51befc
-
-// #114245
-// #5a7d74
-// #c7c2ab
