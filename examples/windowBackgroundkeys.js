21a2e7849c5260f14b35b2234ad2d7a4cd16cbaf
John Romulo Nascimento de Araujo <john.araujo@pampa.compasso>
2021-05-06 16:54:12 -0300

    commit da maldade

--- examples/windowBackgroundkeys.js
+++ examples/windowBackgroundkeys.js
@@ -1,5 +1,4 @@
 const WindowCapture = require("../src/Classes/WindowCapture");
-const cv = require("opencv4nodejs");
 
 // 0x1905fe PokeMMO
 // 0x1905fe PokeMMO
@@ -15,28 +14,6 @@ async function run() {
     let time = new Date().getTime();
 
     const print = await wincap.print();
-
-    const base64Data = print
-      .replace("data:image/jpeg;base64,", "")
-      .replace("data:image/png;base64,", "");
-
-    // await detector.run(Buffer.from(base64Data, "base64"));
-
-    // // console.log("print", print)
-
-    const buffer = Buffer.from(base64Data, "base64");
-    // console.log("buffer", buffer)
-
-    const screenshot = cv.imdecode(buffer, cv.IMREAD_UNCHANGED);
-    console.log("screenshot", screenshot);
-    cv.imshow("Debug", screenshot);
-
-    const key = cv.waitKey(1);
-    if (key === 113) {
-      finish = true;
-      // detector.stop();
-      cv.destroyAllWindows();
-    }
 
     // console.log("screenshot", screenshot);
     // const img = await wincap.print();
